7eab1d471d31d8256d59141fbc7e01c138bc2008
xxq <xuxq1015@163.com>
2018-09-03 17:59:28 +0800

    将港口企业数据存入数据库

--- ghsjzx2Services/src/com/company/Main.java
+++ ghsjzx2Services/src/com/company/Main.java
@@ -35,31 +35,31 @@ public class Main {
 
         /**将数据打印到日志里面*/
         //log.info(ss);
-        System.out.println(ss);
+//        System.out.println(ss);
 
         /**将String数据转化为JSON数据*/
-        //JSONObject responseJson = JSONObject.fromObject(ss);
+        JSONObject responseJson = JSONObject.fromObject(ss);
 
         /**获取data数据*/
-        //Object data = responseJson.get("data");
+        Object data = responseJson.get("data");
        // System.out.println(data);对象不能遍历   [{key:value,key1:value1,...,keyN:valueN},{key:value,key1:value1,...}]
-        //JSONArray jsonArray = JSONArray.fromObject(data );
+        JSONArray jsonArray = JSONArray.fromObject(data );
         //System.out.println(jsonArray); 集合可以遍历   [{key:value,key1:value1,...,keyN:valueN},{key:value,key1:value1,...}]
         /**统计插入数据数量*/
-       /* int count = 0;
+        int count = 0;
         for (int i = 0; i < jsonArray.size(); i++) {
             count++;
             JSONObject jsonObject = JSONObject.fromObject(jsonArray.get(i));
 
-            *//**将查询的数据封装到对象DockWorkInformation*//*
+            /**将查询的数据封装到对象DockWorkInformation*/
             PortEnterpriseData ped = DataTransformUtils.getData(jsonObject);
 
-            *//**对象DockWorkInformation里的数据获取，通过sql存入MySql*//*
+            /**对象DockWorkInformation里的数据获取，通过sql存入MySql*/
             RequestService requestService = new RequestService();
             requestService.saveData(ped);
         }
 
-        System.out.println("本次插入"+count+"条数据.");*/
+        System.out.println("本次插入"+count+"条数据.");
 
     }
 
