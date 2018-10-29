0fda3b96d9912be7ca8158df05567929f6da9a02
xxq <xuxq1015@163.com>
2018-10-29 21:23:20 +0800

    自动生成编号

---
+++ Test.java
@@ -0,0 +1,27 @@
+package com.c503.hthj.asoco.dangerchemical.waste.controller;
+
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @auther xuxq
+ * @date 2018/10/29 21:19
+ */
+public class Test {
+    public static void main(String[] args) {
+
+        Long id = 1L;
+        //
+        DecimalFormat df=new DecimalFormat("00000");
+        String str2=df.format(id);
+
+        //获取系统时间
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+        Date date1 = new Date();
+        String format = sdf.format(date1);
+        //组合编号
+        String code="DZLDS"+format+str2;
+        System.out.println(code);
+    }
+}
