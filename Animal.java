2444c6b3cd0bca9e5eaaf86c4b62b5de97ed6354
xxq <xuxq1015@163.com>
2018-12-19 23:38:00 +0800

    extends下的对象执行顺序---先静后对象、先父后子

---
+++ Animal.java
@@ -0,0 +1,34 @@
+package com.c503.hthj.asoco.dangerchemical.waste.base;
+
+/**
+ * @auther xuxq
+ * @date 2018/12/19 23:30
+ */
+public class Animal {
+    public Animal(){
+        System.out.println("Animal构造方法");
+    }
+    {
+        System.out.println("Animal普通方法方法");
+    }
+    static {
+        System.out.println("Animal静态方法");
+    }
+}
+
+class Dog extends Animal{
+
+    public Dog(){
+        System.out.println("Dog构造方法");
+    }
+    {
+        System.out.println("Dog普通方法");
+    }
+    static {
+        System.out.println("Dog静态方法");
+    }
+    public static void main(String[] args) {
+        new Dog();
+        //先静后对象，先父后子
+    }
+}
