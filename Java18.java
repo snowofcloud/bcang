144035d5d7781aaf7311371f2f0a6c43ed141182
xxq <xuxq1015@163.com>
2018-12-24 21:37:22 +0800

    Java8新特性之方法引用--静态方法引用（Class::static_method）

--- Java18.java
+++ Java18.java
@@ -1,33 +1,33 @@
 package com.xly.spring.boot.mybatisplus.server.controller;
 
+import java.util.Arrays;
+import java.util.function.Consumer;
+
 /**
- * Java8新特性之方法引用--------------构造方法引用（Class::new）
+ * Java8新特性之方法引用---静态方法引用（Class::static_method）
  * @auther xuxq
  * @date 2018/12/24 21:24
  */
 public class Java18 {
     public static void main(String[] args) {
-        A a = C::new;
-        a.test();
-        B b = C::new;
-        b.test("B interface");
-    }
-}
-interface A{
-    void test();
-}
+        //传统方式
+        Arrays.asList("a","b","c").forEach(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                C.sayString(s);
+            }
+        });
 
-interface B{
-    void test(String ss);
+        //引用方式
+        Arrays.asList("a","b","c").forEach(C::sayString);
+    }
 }
 
 class C{
-    private String name;
-    public C(){
 
-    }
+    public C(){ }
 
-    public C(String name){
-        this.name = name;
+    public static void sayString(String s){
+        System.out.println("this is statis ="+s);
     }
 }
