7006f814c74e281b1b2a76007c76e784a5f97ba8
xxq <xuxq1015@163.com>
2018-12-24 21:47:12 +0800

    Java8新特性之方法引用--特定对象方法引用（Class::method）

--- Java18.java
+++ Java18.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.function.Consumer;
 
 /**
- * Java8新特性之方法引用---特定类任意对象方法引用（Class::static_method）
+ * Java8新特性之方法引用---特定对象方法引用（Class::method）
  * @auther xuxq
  * @date 2018/12/24 21:24
  */
@@ -12,13 +12,13 @@ public class Java18 {
     public static void main(String[] args) {
         //List中的对象就是后面传入的方法的参数，
         //所以再C类中参数是一个C类对象
-        Arrays.asList(new C(),new C()).forEach(C::sayString);
+        Arrays.asList("a","b","c").forEach(new C():: sayString);
     }
 }
 
 class C{
 
-    public static void sayString(C c){
-        System.out.println("this is statis ="+c);
+    public  void sayString(String c){
+        System.out.println("this is not statis ="+c);
     }
 }
